86d72abbbf310bb066575f86235d10e944635a52
Muhammad Borham Borham <49664924+MuhammadBorham@users.noreply.github.com>
2025-04-06 19:25:34 +0200

    infrasturcture

---
+++ infra/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "default"
+}
+module "vpc" {
+  source       = "./modules/vpc"
+  cluster_name = var.cluster_name
+  region       = var.region
+}
+
+module "eks" {
+  source       = "./modules/eks"
+  cluster_name = var.cluster_name
+  subnet_ids   = module.vpc.public_subnet_ids
+  k8s_version  = "1.32"
+}
