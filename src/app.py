5146c1d89169681ebf27c943493c0891cfcbbea2
Muhammadborham <mohamedeltier89@gmail.com>
2025-04-14 14:19:46 +0200

    remove last commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
 import uuid
-from prometheus_flask_exporter import PrometheusMetrics
+
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-metrics = PrometheusMetrics(app)
+
 
 # File upload configuration
 app.config['UPLOAD_FOLDER'] = 'static/uploads/books'
@@ -102,14 +102,8 @@ def verify_reset_token(token, expiration=3600):
         return None
     return User.query.filter_by(email=email).first()
 
-#prometheus route
-@app.route('/')
-def home():
-    return "Welcome!"
 
-@app.route('/health')
-def health():
-    return "OK"
+    return "OK", 200
 
 
 # Routes
